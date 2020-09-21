e1378885831061d6922cc5c6942ccfc713ead301
Timur <tgalkovskiy@gmail.com>
2020-09-21 15:53:17 +0300

    Добавил скрипт с возможными ходами(еще нужно кое-что там допилить)

---
+++ Hopeless-Chess/Assets/AI/CameraRotateAround.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotateAround : MonoBehaviour
+{
+
+	public Transform target;
+	public Vector2 startPosition;
+	private Vector3 offset;
+
+	public float sensitivity = 3; // чувствительность мышки
+	public float limit = 80; // ограничение вращения по Y
+	public float zoom = 0.25f; // чувствительность при увеличении, колесиком мышки
+	public float zoomMax = 20; // макс. увеличение
+	public float zoomMin = 5; // мин. увеличение
+	private float X, Y;
+
+	Vector2 tempCursorPosition;
+
+	void Start()
+	{
+		limit = Mathf.Abs(limit);
+		if (limit > 90) limit = 90;
+		X = startPosition.x;
+		Y = - startPosition.y;
+		offset.z = zoomMin;
+	}
+
+	void Update()
+	{
+		if (Input.GetAxis("Mouse ScrollWheel") > 0) offset.z += zoom;
+		else if (Input.GetAxis("Mouse ScrollWheel") < 0) offset.z -= zoom;
+		offset.z = Mathf.Clamp(offset.z, -Mathf.Abs(zoomMax), -Mathf.Abs(zoomMin));
+
+		if (Input.GetMouseButton(1))
+		{
+			//Cursor.visible = false;
+			//Cursor.lockState = CursorLockMode.Locked;
+
+			X = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+			Y += Input.GetAxis("Mouse Y") * sensitivity;
+			Y = Mathf.Clamp(Y, -limit, limit);
+		}
+		else
+		{
+			//Cursor.lockState = CursorLockMode.None;
+			//Cursor.visible = true;
+		}
+
+		transform.localEulerAngles = new Vector3(-Y, X, 0);
+		transform.position = transform.localRotation * offset + target.position;
+	}
+}
---
+++ Hopeless-Chess/Assets/WorkScene2/Scripts/CharacterController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Обрабатывает состояние фигуры в текущий момент времени
+/// </summary>
+public class CharacterController : MonoBehaviour
+{
+    /// <summary>
+    /// Основная информация о персонаже (не изменяется в ходе игры)
+    /// </summary>
+    public CharactersObject character;
+
+    /// <summary>
+    /// Текущий уровень морали фигуры
+    /// </summary>
+    [HideInInspector] public float morality;
+
+    /// <summary>
+    /// Текущий уровень злости (временно)
+    /// </summary>
+    [HideInInspector] public float anger;
+
+    /// <summary>
+    /// Текущий уровень преданности (временно)
+    /// </summary>
+    [HideInInspector] public float devotion;
+    
+    /// <summary>
+    /// Проверка, мёртв ли персонаж
+    /// </summary>
+    public bool isDead;
+
+    /// <summary>
+    /// Проверяет, выбрана ли сейчас эта фигура
+    /// </summary>
+    public bool isSelected;
+
+    public Texture2D moveTexture;
+
+
+    private void Start() 
+    {
+        if(character != null)
+        {
+            morality = character.MaxMorality;
+            anger = character.MaxAnger;
+            devotion = character.MaxDevotion;
+        }
+
+    }
+
+    /// <summary>
+    /// Возвращает скрипт персонажа, включает флаг isSelected
+    /// </summary>
+    /// <returns></returns>
+    public  CharacterController GetCharacter()
+    {
+        isSelected = true;
+        return gameObject.GetComponent<CharacterController>();
+    }
+
+
+    /// <summary>
+    /// Возвращает текстуру хода персонажа (Синий(0;0;255) - ход, если нет препядствий;
+    /// белый (0;0;0) - первый ход за игру;
+    /// зелёный (0;255;0) - безпрепятственный ход
+    /// красный (255;0;0) - ход только в случае атаки)
+    /// </summary>
+    /// <returns></returns>
+    public Texture2D GetMoveTexture()
+    {
+        return moveTexture;
+    }
+
+
+    /// <summary>
+    /// Обрабатывает отказ фигуры ходить в определённом направлении
+    /// </summary>
+    public void GivingUp()
+    {
+        if(morality == 0f)
+        {
+            //...
+        }
+    }
+
+
+    /// <summary>
+    /// Обрабатывает режим ярости фигуры
+    /// </summary>
+    public void RageMode()
+    {
+        if(anger == 0)
+        {
+            //...
+        }
+    }
+
+    /// <summary>
+    /// Обрабатывает предательство фигуры
+    /// </summary>
+    public void Treason()
+    {
+        if(devotion == 0)
+        {
+            //...
+        }
+    }
+
+    /// <summary>
+    /// Обрабатывает перемещение фигуры по доске
+    /// </summary>
+    /// <param name="cellCoords"></param>
+    public void MoveCharacter(Vector3 cellCoords)
+    {
+        transform.position = cellCoords;
+    }
+
+    private enum ChessType
+    {
+        pawn,
+        rook,
+        knight,
+        bishop,
+        queen,
+        king
+    }
+}
